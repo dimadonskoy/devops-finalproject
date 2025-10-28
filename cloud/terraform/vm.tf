301412dc8d289a0478e92278ab74aa9e6b52ad55
Dmitri Donskoy <crooper22@gmail.com>
2025-10-28 21:14:39 +0200

    fixed script deploy

--- cloud/terraform/vm.tf
+++ cloud/terraform/vm.tf
@@ -144,7 +144,7 @@ resource "azurerm_network_security_group" "openwebui" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = [80,443]
+    destination_port_range     = 443
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
