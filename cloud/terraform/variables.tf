e8f98d4ca39f81633fa14f122af5e1c5557202f7
yairk-create <kochavi.yair@gmail.com>
2025-10-30 15:09:11 +0200

    Define variables for Ollama and OpenAI configuration
    Added variables for Ollama user, OpenAI base URL, machine configuration, and GPU settings.

--- cloud/terraform/variables.tf
+++ cloud/terraform/variables.tf
@@ -1,3 +1,36 @@
+# ----------------------
+# Variables
+# ----------------------
+
+variable "ollama_user" {
+  description = "Username to access Ollama"
+  type        = string
+  default     = "admin@demo.gs"
+}
+
+variable "openai_base" {
+  description = "Optional base URL to use with Ollama API"
+  type        = string
+  default     = "https://api.openai.com/v1"
+}
+
+variable "machine" {
+  description = "The machine type and image to use for the VM"
+  type = object({
+    gpu = object({ type = string })
+    cpu = object({ type = string })
+  })
+  default = {
+    gpu = { type = "Standard_NC4as_T4_v3" }
+    cpu = { type = "Standard_A2_v2" }
+  }
+}
+
+variable "gpu_enabled" {
+  description = "Is the VM GPU enabled"
+  type        = bool
+  default     = false
+}
 
 variable "openai_key" {
   description = "OpenAI API key"
