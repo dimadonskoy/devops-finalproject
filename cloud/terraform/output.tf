91fcae6aefaa2ded5bcd181eadb8e96cc6e5917f
Dmitri Donskoy <crooper22@gmail.com>
2025-10-31 09:57:22 +0200

    terraform fixed

--- cloud/terraform/output.tf
+++ cloud/terraform/output.tf
@@ -1,22 +1,14 @@
 
 output "vm_public_ip" {
-  value = azurerm_public_ip.ollama.ip_address
+  value = azurerm_public_ip.openwebui.ip_address
 }
 
 output "private_ip" {
-  value = azurerm_network_interface.ollama.private_ip_address
+  value = azurerm_network_interface.openwebui.private_ip_address
   
 }
 
 output "password" {
   sensitive = true
   value = random_password.password.result
-}
-
-output "ssh_command" {
-  value = "ssh ollama@${azurerm_public_ip.ollama.ip_address}"
-}
-
-output "ollama_api_url" {
-  value = "http://${azurerm_public_ip.ollama.ip_address}:11434"
 }
--- cloud/terraform/vm.tf
+++ cloud/terraform/vm.tf
@@ -1,5 +1,5 @@
-data "azurerm_platform_image" "ollama" {
-  location  = azurerm_resource_group.ollama.location
+data "azurerm_platform_image" "openwebui" {
+  location  = azurerm_resource_group.openwebui.location
   publisher = "Debian"
   offer     = "debian-11"
   sku       = "11"
@@ -20,8 +20,8 @@ data "cloudinit_config" "config" {
 
     content = templatefile("${path.module}/scripts/provision_script.sh",
     {
-      ollama_user = var.ollama_user,
-      ollama_password = random_password.password.result,
+      open_webui_user = var.open_webui_user,
+      open_webui_password = random_password.password.result,
       openai_base = var.openai_base,
       gpu_enabled = var.gpu_enabled,
     }) 
@@ -41,63 +41,63 @@ data "cloudinit_config" "config" {
   }
 }
 
-resource "azurerm_resource_group" "ollama" {
-  name     = "ollama-rg"
+resource "azurerm_resource_group" "openwebui" {
+  name     = "play-ground"
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "ollama" {
-  name                = "ollama-network"
+resource "azurerm_virtual_network" "openwebui" {
+  name                = "example-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.ollama.location
-  resource_group_name = azurerm_resource_group.ollama.name
+  location            = azurerm_resource_group.openwebui.location
+  resource_group_name = azurerm_resource_group.openwebui.name
 }
 
-resource "azurerm_subnet" "ollama" {
+resource "azurerm_subnet" "openwebui" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.ollama.name
-  virtual_network_name = azurerm_virtual_network.ollama.name
-  address_prefixes     = [cidrsubnet(azurerm_virtual_network.ollama.address_space[0], 8, 2)]
+  resource_group_name  = azurerm_resource_group.openwebui.name
+  virtual_network_name = azurerm_virtual_network.openwebui.name
+  address_prefixes     = [cidrsubnet(azurerm_virtual_network.openwebui.address_space[0], 8, 2)]
 }
 
-resource "azurerm_public_ip" "ollama" {
-  name                = "ollama-ip"
-  location            = azurerm_resource_group.ollama.location
-  resource_group_name = azurerm_resource_group.ollama.name
+resource "azurerm_public_ip" "openwebui" {
+  name                = "openwebui-ip"
+  location            = azurerm_resource_group.openwebui.location
+  resource_group_name = azurerm_resource_group.openwebui.name
 
   allocation_method   = "Static"   # or "Dynamic"
   sku                 = "Standard" # <- FIX: use Standard instead of Basic
 }
 
 
-resource "azurerm_network_interface" "ollama" {
-  name                = "ollama-nic"
-  location            = azurerm_resource_group.ollama.location
-  resource_group_name = azurerm_resource_group.ollama.name
+resource "azurerm_network_interface" "openwebui" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.openwebui.location
+  resource_group_name = azurerm_resource_group.openwebui.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.ollama.id
+    subnet_id                     = azurerm_subnet.openwebui.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.ollama.id
+    public_ip_address_id = azurerm_public_ip.openwebui.id
   }
 }
 
 
 
 
-resource "azurerm_linux_virtual_machine" "ollama" {
-  name                = "ollama-machine"
-  resource_group_name = azurerm_resource_group.ollama.name
-  location            = azurerm_resource_group.ollama.location
+resource "azurerm_linux_virtual_machine" "openwebui" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.openwebui.name
+  location            = azurerm_resource_group.openwebui.location
   size                = var.gpu_enabled ? var.machine.gpu.type : var.machine.cpu.type
-  admin_username      = "ollama"
+  admin_username      = "openwebui"
   network_interface_ids = [
-    azurerm_network_interface.ollama.id,
+    azurerm_network_interface.openwebui.id,
   ]
 
   admin_ssh_key {
-    username   = "ollama"
+    username   = "openwebui"
     public_key = var.ssh_public_key
   }
 
@@ -107,10 +107,10 @@ resource "azurerm_linux_virtual_machine" "ollama" {
   }
 
   source_image_reference {
-    publisher = data.azurerm_platform_image.ollama.publisher
-    offer     = data.azurerm_platform_image.ollama.offer
-    sku       = data.azurerm_platform_image.ollama.sku
-    version   = data.azurerm_platform_image.ollama.version
+    publisher = data.azurerm_platform_image.openwebui.publisher
+    offer     = data.azurerm_platform_image.openwebui.offer
+    sku       = data.azurerm_platform_image.openwebui.sku
+    version   = data.azurerm_platform_image.openwebui.version
   }
 
   custom_data = data.cloudinit_config.config.rendered
@@ -120,10 +120,10 @@ resource "azurerm_linux_virtual_machine" "ollama" {
 
 # Network Security Group 
 
-resource "azurerm_network_security_group" "ollama" {
-  name                = "ollama-nsg"
-  location            = azurerm_resource_group.ollama.location
-  resource_group_name = azurerm_resource_group.ollama.name
+resource "azurerm_network_security_group" "openwebui" {
+  name                = "webserver"
+  location            = azurerm_resource_group.openwebui.location
+  resource_group_name = azurerm_resource_group.openwebui.name
 
   security_rule {
     name                       = "SSH"
@@ -138,7 +138,7 @@ resource "azurerm_network_security_group" "ollama" {
   }
 
   security_rule {
-    name                       = "HTTPS"
+    name                       = "Http"
     priority                   = 110
     direction                  = "Inbound"
     access                     = "Allow"
@@ -150,20 +150,8 @@ resource "azurerm_network_security_group" "ollama" {
   }
 
   security_rule {
-    name                       = "Ollama_API"
-    priority                   = 120
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = 11434
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
     name                       = "WEB_OUT"
-    priority                   = 130
+    priority                   = 120
     direction                  = "Outbound"
     access                     = "Allow"
     protocol                   = "Tcp"
@@ -174,8 +162,8 @@ resource "azurerm_network_security_group" "ollama" {
   }
 }
 
-resource "azurerm_subnet_network_security_group_association" "ollama" {
-  subnet_id                 = azurerm_subnet.ollama.id
-  network_security_group_id = azurerm_network_security_group.ollama.id
+resource "azurerm_subnet_network_security_group_association" "openwebui" {
+  subnet_id                 = azurerm_subnet.openwebui.id
+  network_security_group_id = azurerm_network_security_group.openwebui.id
   
 }
